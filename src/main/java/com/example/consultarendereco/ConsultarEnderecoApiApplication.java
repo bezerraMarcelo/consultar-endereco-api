8653b3909becbc21a8c2f9b28924fb21dc98cdfd
bezerraMarcelo <marcelo.abezerra@gmail.com>
2025-05-17 22:10:42 -0300

    Criação do projeto

---
+++ src/main/java/com/example/consultarendereco/ConsultarEnderecoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.consultarendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultarEnderecoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultarEnderecoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/consultarendereco/ConsultarEnderecoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consultarendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultarEnderecoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
